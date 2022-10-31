8c1a77ec0249f4d3254a23af6d2dd03ef43fc13c
ks-grshkv <kseniia.grshkv@yandex.ru>
2022-10-31 17:21:15 +0300

    Added database url parser

--- config/database.ts
+++ config/database.ts
@@ -7,10 +7,10 @@
 
 import Env from '@ioc:Adonis/Core/Env'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
-import Logger from '@ioc:Adonis/Core/Logger'
 
 
-Logger.info('A info message')
+const Url = require('url-parse')
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 const databaseConfig: DatabaseConfig = {
   /*
@@ -40,11 +40,11 @@ const databaseConfig: DatabaseConfig = {
     pg: {
       client: 'pg',
       connection: {
-        host: Env.get('PG_HOST'),
-        port: Env.get('PG_PORT'),
-        user: Env.get('PG_USER'),
-        password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
+        host: Env.get('PG_HOST', DATABASE_URL.hostname),
+        port: Env.get('PG_PORT', DATABASE_URL.port),
+        user: Env.get('PG_USER', DATABASE_URL.username),
+        password: Env.get('PG_PASSWORD', DATABASE_URL.password),
+        database: Env.get('PG_DB_NAME', DATABASE_URL.pathname.substr(1)),
         ssl: {
           rejectUnauthorized: false,
         },
