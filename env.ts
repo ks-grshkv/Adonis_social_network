5bdb3e477a5986d09e16024c1097259e11e8d7b7
ks-grshkv <kseniia.grshkv@yandex.ru>
2022-10-31 16:45:58 +0300

    PostgreSQL connected

--- env.ts
+++ env.ts
@@ -25,9 +25,10 @@ export default Env.rules({
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
 
   DB_CONNECTION: Env.schema.string(),
-  MYSQL_HOST: Env.schema.string({ format: 'host' }),
-  MYSQL_PORT: Env.schema.number(),
-  MYSQL_USER: Env.schema.string(),
-  MYSQL_PASSWORD: Env.schema.string.optional(),
-  MYSQL_DB_NAME: Env.schema.string(),
+
+  PG_HOST: Env.schema.string({ format: 'host' }),
+  PG_PORT: Env.schema.number(),
+  PG_USER: Env.schema.string(),
+  PG_PASSWORD: Env.schema.string.optional(),
+  PG_DB_NAME: Env.schema.string(),
 })
